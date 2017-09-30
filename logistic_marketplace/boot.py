bc50640bf7729b7c6287e9a8ee45db6ae67c11fb
Bobby <bobzz.zone@gmail.com>
2017-09-30 07:43:06 +0000

    aa

--- logistic_marketplace/boot.py
+++ logistic_marketplace/boot.py
@@ -9,12 +9,12 @@ from frappe.utils.nestedset import get_root_of
 def boot_session(bootinfo):
 	has_role = frappe.db.sql("""select allow,for_value from `tabUser Permission` where user="{}" """.format(frappe.session['user']),as_dict=1)
 	for row in has_role:
-		if row.['allow'] == "Principle":
+		if row['allow'] == "Principle":
 			frappe.session['principle']=row['for_value']
 			bootinfo.sysdefaults.principle=row['for_value']
-		elif row.['allow'] == "Vendor":
+		elif row['allow'] == "Vendor":
 			frappe.session['vendor']=row['for_value']
 			bootinfo.sysdefaults.vendor=row['for_value']
-		elif row.['allow'] == "Driver":
+		elif row['allow'] == "Driver":
 			frappe.session['driver']=row['for_value']
 			bootinfo.sysdefaults.driver=row['for_value']
