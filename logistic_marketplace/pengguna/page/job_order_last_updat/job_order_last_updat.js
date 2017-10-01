293e42f976fd0c8c793c8ea3374e040b655252fd
bobzz-zone <bobzz.zone@gmail.com>
2017-10-01 20:19:07 +0700

    a

--- logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
+++ logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
@@ -1,6 +1,3 @@
-var script = document.createElement('script');
-script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-document.body.appendChild(script);
 var map;
 var bounds;
 
@@ -16,6 +13,7 @@ frappe.pages['job-order-last-updat'].on_page_load = function(wrapper) {
 MapLastUpdate = Class.extend({
 	init: function(wrapper) {
 		var me = this;
+		$('<script async defer src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initMap"></script><div id="map" class="map"></div>').appendTo($(wrapper).find(".layout-main"))
 		// 0 setTimeout hack - this gives time for canvas to get width and height
 		setTimeout(function() {
 			me.setup(wrapper);
@@ -24,16 +22,17 @@ MapLastUpdate = Class.extend({
 	},
 	setup: function(wrapper) {
 		var me = this;
-	    bounds = new google.maps.LatLngBounds();
-	    var mapOptions = {
-	    				    mapTypeId: 'roadmap',
-							center: {lat:-7.3111249,lng:112.7279283},
-							zoom: 10
-		    			};
-                    
-    	// Display a map on the page
-	    map = new google.maps.Map($(wrapper).find(".layout-main"), mapOptions);
-    	map.setTilt(45);
+		bounds=1;
+		// bounds = new google.maps.LatLngBounds();
+		// var mapOptions = {
+		// 					mapTypeId: 'roadmap',
+		// 					center: {lat:-7.3111249,lng:112.7279283},
+		// 					zoom: 10
+		// 				};
+					
+		// // Display a map on the page
+		// map = new google.maps.Map($(wrapper).find(".map"), mapOptions);
+		// map.setTilt(45);
 
 		principle_ro=0
 		if (frappe.session.principle){
@@ -66,64 +65,64 @@ MapLastUpdate = Class.extend({
 		};
 				
 	},
-	get_data: function(btn) {
-		var me = this;
-		frappe.call({
-			method: "logistic_marketplace.pengguna.page.job_order_last_updat.job_order_last_update.get_job_order_data",
-			args: {
-				principle: this.options.principle,
-				vendor: this.options.vendor
-			},
-			btn: btn,
-			callback: function(r) {
-				if(!r.exc) {
-					me.options.data = r.message;
-					me.render();
-				}
-			}
-		});
-	},
-	render: function() {
-		var me = this;
-		data = me.options.data;
-		markers = [];
-		infoWindowContent = [];
-		for (var row in data){
-			info = data[row];
-			markers.push([info.job_order,info.lat,info.lo]);
-			infoWindowContent.push(['<div class="info_content">' +
-				'<h3>'+ info.job_order+'</h3>' +
-				'<p>Principle : '+info.principle+'</p>'+
-				'<p>Vendor : '+info.vendor+'</p></div>']);
-		}
+	// get_data: function(btn) {
+	// 	var me = this;
+	// 	frappe.call({
+	// 		method: "logistic_marketplace.pengguna.page.job_order_last_updat.job_order_last_update.get_job_order_data",
+	// 		args: {
+	// 			principle: this.options.principle,
+	// 			vendor: this.options.vendor
+	// 		},
+	// 		btn: btn,
+	// 		callback: function(r) {
+	// 			if(!r.exc) {
+	// 				me.options.data = r.message;
+	// 				me.render();
+	// 			}
+	// 		}
+	// 	});
+	// },
+	// render: function() {
+	// 	var me = this;
+	// 	data = me.options.data;
+	// 	markers = [];
+	// 	infoWindowContent = [];
+	// 	for (var row in data){
+	// 		info = data[row];
+	// 		markers.push([info.job_order,info.lat,info.lo]);
+	// 		infoWindowContent.push(['<div class="info_content">' +
+	// 			'<h3>'+ info.job_order+'</h3>' +
+	// 			'<p>Principle : '+info.principle+'</p>'+
+	// 			'<p>Vendor : '+info.vendor+'</p></div>']);
+	// 	}
 			
-		// Display multiple markers on a map
-		var infoWindow = new google.maps.InfoWindow(), marker, i;
+	// 	// Display multiple markers on a map
+	// 	var infoWindow = new google.maps.InfoWindow(), marker, i;
    
-		// Loop through our array of markers & place each one on the map  
-		for( i = 0; i < markers.length; i++ ) {
-			var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-			bounds.extend(position);
-			marker = new google.maps.Marker({
-				position: position,
-				map: map,
-				title: markers[i][0]
-			});
-       
-			// Allow each marker to have an info window    
-			google.maps.event.addListener(marker, 'click', (function(marker, i) {
-				return function() {
-					infoWindow.setContent(infoWindowContent[i][0]);
-					infoWindow.open(map, marker);
-				}
-			})(marker, i));
-				// Automatically center the map fitting all markers on the screen
-			map.fitBounds(bounds);
-		}
-			// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-		var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-			this.setZoom(7);
-			google.maps.event.removeListener(boundsListener);
-		});
-	}
+	// 	// Loop through our array of markers & place each one on the map  
+	// 	for( i = 0; i < markers.length; i++ ) {
+	// 		var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+	// 		bounds.extend(position);
+	// 		marker = new google.maps.Marker({
+	// 			position: position,
+	// 			map: map,
+	// 			title: markers[i][0]
+	// 		});
+	   
+	// 		// Allow each marker to have an info window	
+	// 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	// 			return function() {
+	// 				infoWindow.setContent(infoWindowContent[i][0]);
+	// 				infoWindow.open(map, marker);
+	// 			}
+	// 		})(marker, i));
+	// 			// Automatically center the map fitting all markers on the screen
+	// 		map.fitBounds(bounds);
+	// 	}
+	// 		// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+	// 	var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+	// 		this.setZoom(7);
+	// 		google.maps.event.removeListener(boundsListener);
+	// 	});
+	// }
 });
