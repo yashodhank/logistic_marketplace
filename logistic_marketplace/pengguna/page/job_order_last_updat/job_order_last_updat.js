8421a7b1b0440a676383f1d73efe22392bb101d2
bobzz-zone <bobzz.zone@gmail.com>
2017-10-02 01:10:28 +0700

    tes

--- logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
+++ logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
@@ -79,6 +79,7 @@ MapLastUpdate = Class.extend({
 		};
 		$.each(this.options, function(k, v) {
 			//me.elements[k].val(frappe.datetime.str_to_user(v));
+			me.elements[k].val(v);
 			me.elements[k].on("change", function() {
 				me.options[k] = $(this).val();
 				me.get_data();
