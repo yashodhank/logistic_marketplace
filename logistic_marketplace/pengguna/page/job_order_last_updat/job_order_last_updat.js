f4e0c0b4f7b1b47fb504d643c83d5227f5f8b7d8
bobzz-zone <bobzz.zone@gmail.com>
2017-10-02 01:25:56 +0700

    a

--- logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
+++ logistic_marketplace/pengguna/page/job_order_last_updat/job_order_last_updat.js
@@ -96,9 +96,10 @@ MapLastUpdate = Class.extend({
 			method: "logistic_marketplace.pengguna.page.job_order_last_updat.job_order_last_update.get_job_order_data",
 			args: {
 				filter:{
-				principle: this.options.principle,
-				vendor: this.options.vendor
-			}
+					principle: this.options.principle,
+					vendor: this.options.vendor,
+					dummy:"just_for_dummy"
+				}
 			},
 			btn: btn,
 			callback: function(r) {
