49b83ab6526c520c3992507db8f3c3a83e0d8f00
bobzz-zone <bobzz.zone@gmail.com>
2017-10-02 01:36:28 +0700

    gg

--- logistic_marketplace/config/desktop.py
+++ logistic_marketplace/config/desktop.py
@@ -24,5 +24,12 @@ def get_data():
 			"icon": "octicon octicon-gist-secret",
 			"type": "module",
 			"label": _("Pengguna")
+		},
+		{
+			"name": "job-order-last-updat",
+			"color": "grey",
+			"icon": "octicon octicon-checklist",
+			"type": "page",
+			"label": _("Job Order Last Update Position")
 		}
 	]
