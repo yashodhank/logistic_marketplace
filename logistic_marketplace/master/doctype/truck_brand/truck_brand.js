0eb3e9ee81b356430f7c240d5593019633c4db7d
Bobby <bobzz.zone@gmail.com>
2017-10-07 03:31:10 +0000

    aa

---
+++ logistic_marketplace/master/doctype/truck_brand/truck_brand.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, bobzz.zone@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Truck Brand', {
+	refresh: function(frm) {
+
+	}
+});
