55b4cfe3a63266bce97edcb91a8e3fa1bb92a779
bobzz-zone <bobzz.zone@gmail.com>
2017-11-03 20:42:02 +0700

    a

--- logistic_marketplace/pengguna/page/live_map/live_map.py
+++ logistic_marketplace/pengguna/page/live_map/live_map.py
@@ -4,13 +4,15 @@ import frappe
 from frappe import _
 
 @frappe.whitelist()
-def get_position(principle,vendor):
+def get_position(principle,vendor,driver):
 	where = ""
 	if principle!="All":
 		where = """ and p.principle ="{}" """.format(principle)
 	if vendor!="All":
 		where = """{} and p.vendor="{}" """.format(where,vendor)
-	result = frappe.db.sql("""select p.name , d.last_update,d.lo,d.lat,p.principle ,p.vendor ,d.driver 
+	if driver!="All":
+		where = """{} and p.driver="{}" """.format(where,driver)
+	result = frappe.db.sql("""select p.name , d.last_update,d.lo,d.lat,p.principle ,p.vendor ,p.driver 
 		join `tabJob Order` p 
 		join `tabDriver` d on d.name = p.driver 
 		where p.status = "Dalam Proses" {}  order by d.last_update desc """.format(where),as_dict=1)
