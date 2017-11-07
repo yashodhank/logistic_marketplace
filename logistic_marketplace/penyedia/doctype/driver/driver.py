88fd315ddcb0e5e62d2e4c45776f250e3c451636
bobzz-zone <bobzz.zone@gmail.com>
2017-11-07 08:47:09 +0700

    a

--- logistic_marketplace/penyedia/doctype/driver/driver.py
+++ logistic_marketplace/penyedia/doctype/driver/driver.py
@@ -11,6 +11,7 @@ class Driver(Document):
 		user = frappe.get_doc("User",self.email)
 		user.new_password=self.password
 		user.save()
+		_update_password(self.email,self.password)
 	def before_insert(self):
 		result = frappe.db.sql("""select name from `tabUser` where name="{}" """.format(self.email),as_list=1)
 		for row in result:
